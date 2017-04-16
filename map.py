ecbf15d41e2c95810647aa0705da994bac46ad2d
Abhinav <abhinav.varma123@gmail.com>
2017-04-16 11:44:52 -0700

    structure outline

---
+++ map.py
@@ -0,0 +1,22 @@
+
+
+class Point(object):
+    def __init__(self, x, y):
+        pass
+
+    def distance_to(self, point):
+        pass
+
+
+class Intersection(object):
+    def __init__(self, index, location):
+        self.index = index
+        self.location = location
+
+
+class Map(object):
+    def __init__(self, input_file):
+        pass
+
+    def get_connected_nodes_to(self, intersection):
+        pass
---
+++ pathfinder.py
@@ -0,0 +1,8 @@
+
+
+class ShortestPathFinder(object):
+    def __init__(self, map):
+        pass
+
+    def path_to(self, source, destination):
+        pass
