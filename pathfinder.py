0c9e0f925b13aa40712145e8c0f30b1858ebb196
Abhinav <abhinav.varma123@gmail.com>
2017-04-16 12:05:14 -0700

    shortest path algo cache outline

--- pathfinder.py
+++ pathfinder.py
@@ -1,8 +1,17 @@
-
-
 class ShortestPathFinder(object):
     def __init__(self, map):
         pass
 
-    def path_to(self, source, destination):
+    def _compute_single_source_shortest_path(self, source):
         pass
+
+    def get_cached_paths(self, source):
+        return {}
+
+    def get_shortest_path(self, source, destination):
+        cached_paths = self.get_cached_paths(source)
+        if not cached_paths:
+            self._compute_single_source_shortest_path(source)
+            cached_paths = self.get_cached_paths(source)
+
+        return cached_paths[destination]
