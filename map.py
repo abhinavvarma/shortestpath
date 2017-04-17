2d9890f5e4daf6f5ad2e5dc172ce159c2bc9f674
Shyam Sundar <dssundar@hotmail.com>
2017-04-16 20:29:00 -0700

    Implemented add intersection & routes, getting instances of connected routes

--- map.py
+++ map.py
@@ -22,7 +22,8 @@ class Intersection(object):
 
 class Map(object):
     def __init__(self, input_file):
-        self.nodes = {}
+        self.routes = {}
+        self.intersection_list = []
 
     def load_input(self, input_file):
         '''
@@ -30,7 +31,7 @@ class Map(object):
         :param input_file: path to the input file 
         :return: 
         '''
-        file = open('data/input6.txt', 'r')
+        file = open(input_file, 'r')
 
         value = file.readline().strip().split(' ')
 
@@ -50,17 +51,69 @@ class Map(object):
             x_coord = coordinates[1]
             y_coord = coordinates[2]
 
-            point = map.Point(x_coord, y_coord)
-            intersection = map.Intersection(index, point)
-            intersections_list.append(intersection)
+
+            #add intersection to the intersection list
+            self.add_intersection(index, x_coord, y_coord)
 
 
 
-    def addNodes(self, index):
-        pass
+
+        #reading the route between the points
+        connected_intersections = file.readline().strip().split()
+
+        while connected_intersections != []:
+            self.add_route(connected_intersections[0], connected_intersections[1])
+            connected_intersections = file.readline().strip().split()
+
+
+
+    def add_intersection(self, index, x_coord, y_coord):
+        '''
+        Create a new intersection, point instance and add to a list of instances
+        :param index: index of the location 
+        :param x_coord: x coordinate of location
+        :param y_coord: y coordinate of location
+        :return: None
+        '''
+        #initialize the route dict for each index
+        self.route[index] = []
+
+        #create a new point instance
+        new_point = Point(x_coord, y_coord)
+
+        #create a new intersectio instance
+        new_intersection = Intersection(index, new_point)
+
+        #append the instance of the intersection to a list
+        #list index represents index of the location
+        self.intersection_list.append(new_intersection)
+
+
+
+    def add_route(self, start, end):
+        '''
+        Add a route from start to ending index, the routes are stored as a dictionary
+        :param start: index of starting location 
+        :param end: index of ending location
+        :return: 
+        '''
+        if end not in self.routes[start]:
+            self.routes[start].append(end)
+
+
+
 
     def get_connected_intersections_to(self, intersection):
-        pass
+        '''
+        Return the instance of intersections as a list
+        :param intersection: integer value of the intersection
+        :return: list of instances 
+        '''
+        connected_indexes = self.intersection_list[intersection]
+        intersection_instance_list = []
+        for index in connected_indexes:
+            intersection_instance_list.append(self.intersection_list[index])
 
-    def get_intersections(self):
-        pass
+        return intersection_instance_list
+
+
