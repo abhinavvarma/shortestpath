6c3abd3c73c1d452e6efc135e4335405b288cd36
Shyam Sundar <dssundar@hotmail.com>
2017-05-05 21:38:17 -0700

    visualization

--- driver.py
+++ driver.py
@@ -6,9 +6,9 @@ file = 'data/input6.txt'
 
 map_instance = map.Map()
 map_instance.load_input(file)
+result = ShortestPathFinder(map_instance).get_shortest_path( map_instance.get_intersection(0),
+                                                             map_instance.get_intersection(5),)
 
-result = ShortestPathFinder(map_instance).get_shortest_path(
-    map_instance.get_connected_intersections_to(0),
-    map_instance.get_connected_intersections_to(5),
-)
-plot_points(map_instance.intersection_list)
+print(result[1])
+
+plot_points(map_instance.intersection_list,result[1])
--- visualizepoints.py
+++ visualizepoints.py
@@ -3,12 +3,21 @@ import matplotlib.pyplot as plt
 x = []
 y = []
 
-def plot_points(intersection_points):
+
+x_all = []
+y_all = []
+
+def plot_points(all_instances, intersection_points):
     for point in intersection_points:
        x.append(point.location.x_coord)
        y.append(point.location.y_coord)
 
+    for all_point in all_instances:
+        x_all.append(all_point.location.x_coord)
+        y_all.append(all_point.location.y_coord)
 
+
+    plt.scatter(x_all,y_all)
     plt.scatter(x,y)
     plt.plot(x,y)
 
