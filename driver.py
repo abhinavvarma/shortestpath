89397a7652332c532f00cde49badbe017dfe4fde
Abhinav <abhinav.varma123@gmail.com>
2017-05-07 23:47:00 -0700

    execution script

--- driver.py
+++ driver.py
@@ -7,11 +7,30 @@ file = 'data/usa.txt'
 
 map_instance = map.Map()
 map_instance.load_input(file)
-# result = AStarShortestPathFinder(map_instance).get_shortest_path( map_instance.get_intersection(0),
-#                                                              map_instance.get_intersection(5),)
-result = AStarShortestPathFinder(map_instance).get_shortest_path( map_instance.get_intersection(29),
-                                                             map_instance.get_intersection(87411),)
 
+source_index = raw_input("\nEnter the index of the source intersection. (Hit enter to choose the default value)")
+if not source_index:
+    source_index = 608
+
+source = map_instance.get_intersection(source_index)
+print "Selected Source:", str(source)
+
+dest_index = raw_input("\nEnter the index of the destination intersection. (Hit enter to choose the default value)")
+if not dest_index:
+    dest_index = 76344
+
+dest = map_instance.get_intersection(dest_index)
+print "Destination:", str(dest)
+
+algo_choice = raw_input("\nPlease choose the algorithm to run. \n1. A-Star Algorithm \n2. Dijkstra's Algorithm\n(Hit enter to choose the default algorithm)\n")
+if (algo_choice==1):
+    Algo = DijkstraShortestPathFinder
+else:
+    Algo = AStarShortestPathFinder
+
+print "Selected Algorithm:", Algo
+print "\nExecuting.."
+result = Algo(map_instance).get_shortest_path(source,dest)
 
 v = Visualizer()
 v.draw(map_instance, result[1])
